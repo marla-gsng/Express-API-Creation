fdeaa7b974d3176ecbee16a0062f4dc70176c841
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-03-19 15:29:25 +0100

    made own api using express and checking with postman

---
+++ index.js
@@ -0,0 +1,80 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening in on port ${port}`);
+});
+
+const wonderland = [
+  {
+    id: 1,
+    name: "Alice",
+    kind: "human",
+  },
+  {
+    id: 2,
+    name: "Mad Hatter",
+    kind: "human",
+  },
+  {
+    id: 3,
+    name: "Cheshire Cat",
+    kind: "cat",
+  },
+  {
+    id: 4,
+    name: "Queen of Hearts",
+    kind: "human",
+  },
+  {
+    id: 5,
+    name: "White Rabbit",
+    kind: "rabbit",
+  },
+  {
+    id: 6,
+    name: "Caterpillar",
+    kind: "caterpillar",
+  },
+  {
+    id: 7,
+    name: "Duchess",
+    kind: "human",
+  },
+  {
+    id: 8,
+    name: "Tweedledee",
+    kind: "human",
+  },
+  {
+    id: 9,
+    name: "Tweedledum",
+    kind: "human",
+  },
+];
+
+app.get("/api/wonderland", (req, res) => {
+  res.send(wonderland);
+});
+
+app.get("/api/wonderland/:id", (req, res) => {
+  let id = req.params.id;
+  let character = wonderland.find((character) => character.id == parseInt(id));
+  res.send(character);
+});
+
+app.post("/api/wonderland", (req, res) => {
+  const character = {
+    id: wonderland.length + 1,
+    name: req.body.name,
+    kind: req.body.kind,
+  };
+  wonderland.push(character);
+  res.send(character);
+});
